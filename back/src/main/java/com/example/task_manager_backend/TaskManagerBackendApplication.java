b73c448c851b854adf7c89aa2ee3865c7eb874f5
yahyasghayron <yahya.sghayron123@gmail.com>
2024-08-31 21:20:04 +0100

    init backen project

---
+++ back/src/main/java/com/example/task_manager_backend/TaskManagerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.task_manager_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagerBackendApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/com/example/task_manager_backend/TaskManagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.task_manager_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
